ac98d21e1d84947166bc04047c38e83f1e4b9b09
your name <20783506632@qq.com>
2018-04-28 10:08:07 +0800

    基础搭建

---
+++ src/pages/ShoppingMall.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      {{msg}}
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { Button, Row, Col } from 'vant'
+Vue.use(Button).use(Row).use(Col)
+export default {
+  data () {
+    return {
+      msg: 'Shopping Mall'
+    }
+  }
+}
+</script>
